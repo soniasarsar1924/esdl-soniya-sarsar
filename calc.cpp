03e4fe5956e75b1bd527cf6b6a09c95104415f6d
soniasarsar1924 <Soniya.Sarsar@cumminscollege.in>
2014-07-28 11:28:03 +0530

    Update calc.cpp
    add sub() method.

--- calc.cpp
+++ calc.cpp
@@ -9,3 +9,10 @@ double add (double in_number1,double in_number2)
 {
   return in_number1+in_number2;
 }
+
+
+
+double sub (double in_number1,double in_number2)
+{
+  return in_number1-in_number2;
+}
